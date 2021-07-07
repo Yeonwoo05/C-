09cf45040d06de25d8a4bc48bf24d3723d3fe20e
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-07-08 08:21:22 +0900

    Add files via upload

---
+++ 02-01-03_기본데이터.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+// > 부동 소수점?
+// - 소수점이 고정되어있지 않고 움직이면서 수를 표현한다는 뜻에서 지어진 이름
+// [데이터 형식]    [크기 (byte) ]   [설명]
+//		float			4			단일 정밀도 부동 소수점 형식 / 유효자리(7자리)
+//		double			8			복수 정밀도 부동 소수점 형식 / 유효자리(15~16자리)
+//		decimal			16			유효자리(29)
+
+namespace _02_변수._01_기본데이터
+{
+	class _03_기본데이터
+	{
+		static void Main()
+		{
+			// float
+			// > 값 뒤에 'f', 'F'를 붙여서 float형 데이터를 표현합니다.
+			float MyFloat = 3.14f;
+			Console.WriteLine("MyFloat = " + MyFloat);
+
+			// > double
+			// double 'd', 'D'를 붙여서 표현을 해도 되지만
+			// 굳이 작성하지 않아도 double데이터로 인식합니다.
+			double MyDouble = 123.4567;
+			Console.WriteLine("MyDouble = " + MyDouble);
+
+			// > Decimal
+			// decimal은 'm', 'M'을 붙여서 표현합니다.
+			decimal MyDecimal = 0.1234567890m;
+			Console.WriteLine("MyDecimal = " + MyDecimal);
+		}
+	}
+}
---
+++ 02-01-04_기본데이터.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// 문자 형식에는 char 타입을 사용합니다.
+// - 문자의 앞 뒤에는 작은 따옴표(') 를 사용해서 char형 데이터임을 표현합니다.
+namespace _02_변수._01_기본데이터
+{
+	class _04_기본데이터
+	{
+		static void Main()
+		{
+			char MyCharacter1 = '문';
+			char MyCharacter2 = '자';
+			Console.WriteLine("MyCharacter1 = " + MyCharacter1);
+			Console.WriteLine("MyCharacter2 = " + MyCharacter2);
+
+			Console.WriteLine("MyCharacter1 to int = " + (int)MyCharacter1);
+			Console.WriteLine("MyCharacter2 to int = " + (int)MyCharacter2);
+
+			// > string
+			// - 문자들이 연속해서 가지런히 놓여있는 줄을 의미합니다.
+			// - 문자열이라고 합니다.
+			// - 문자열은 큰 따옴표(")를 사용해서 string형 데이터를 표현합니다.
+
+			string MyString1 = "안녕하세요.";
+			string MyString2 = "재밌는 C#입니다.";
+
+			Console.WriteLine("MyString1 = " + MyString1);
+			Console.WriteLine("MyString2 = " + MyString2);
+
+		}
+	}
+}
