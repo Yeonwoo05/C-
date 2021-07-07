28d06213aa58c8e86c631650ccdac6842184108c
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-07-07 17:50:18 +0900

    Add files via upload

---
+++ 03_출력.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_출력
+{
+	class _03_출력
+	{
+		// 출력개행을 이용해서
+		// 자신의 이름과 포부를 적어봅시다.
+		// 나는 박진혁이다! 나는 오늘부터 개발을 열심히 할꺼다!
+		static void Main()
+		{
+			Console.WriteLine("나는 박진혁이다! 나는 오늘부터 개발을 열심히 할꺼다!");
+		}
+	}
+}
