cdf11a272fa647fe57e49e370f94d22607ebee9f
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-07-18 12:41:46 +0900

    Add files via upload

---
+++ 03-05-03_Null조건부연산자.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// > Null 조건부 연산자?
+/// - 접근하려는 값이 null 인지 검사하여 그 결과가 null 이라면 null을 반환
+///   null 이 아니라면 접근하려는 값을 반환하는 연산자입니다.
+
+namespace _03_연산자._05_조건연산자
+{
+	class _03_Null조건부연산자
+	{
+		static void Main()
+		{
+			int a = 10;
+			object nullObject = null;
+			object myObject = a;/*nullObject?.ToString();*/
+
+			Console.WriteLine("myObject is Not Value ? " +
+				((myObject == null) ? "YES" : "No, my ObjectValue =" +
+				myObject));
+		}
+	}
+}
