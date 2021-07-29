e3270477111f17ee30621910f9f6c90465c7b44f
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-07-29 13:58:07 +0900

    Add files via upload

---
+++ 05-01-06_while문.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// > 구구단을 1단부터 9단까지 출력해주세요
+						/// - 1단 - 
+						/// 1 X 1 = 1
+						/// 1 X 2 = 2
+						/// ...
+						/// 1 X 9 = 9
+						/// - 2단 -
+						/// 2 X 1 = 2
+						/// 2 X 2 = 4
+						/// ....
+						/// - 9단 -
+						/// 9 X 1 = 9
+						/// 9 X 2 = 18
+						/// ...
+						/// 9 X 9 = 81
+
+class _06_While문
+{
+	static void Main()
+	{
+		int N = 1;
+		while (N < 10)
+		{
+			Console.WriteLine($" - {N}단 - ");
+			int M = 1;
+			while (M < 10)
+				Console.WriteLine($"{N} X {M} = {(N * M++)}");
+			N++;
+
+			Console.WriteLine();
+		}
+	}
+}
