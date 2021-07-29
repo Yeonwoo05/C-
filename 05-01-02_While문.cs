63a8966118aaafc15abff40a0f64276832890bf5
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-07-29 13:57:31 +0900

    Add files via upload

---
+++ 05-01-02_While문.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class _02_While문
+{
+	static void Main()
+	{
+		// 1 ~ 10까지 수를 출력하는 반복문
+		int count = 1;
+		while (count <= 10) Console.WriteLine(count++);
+
+		Console.WriteLine();
+		// 1 ~ 50까지 짝수만 출력
+		count = 1;
+		while(count <= 50)
+		{
+			if (count % 2 == 0) Console.WriteLine("count = " + count);
+			count++;
+		}
+
+	}
+}
