cdfc7f229ae7d5a69df0ed81b2953cb15c9a1161
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-07-29 13:57:47 +0900

    Add files via upload

---
+++ 05-01-04_While문.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class _04_While문
+{
+	// while문을 이용해서 1 ~ 100까지의 합을 구해보세요.
+
+	static void Main()
+	{
+		int count = 1;
+
+		// 결과를 저장할 변수
+		int result = 0;
+
+		while (count <= 100) result += (count++);
+
+		Console.WriteLine("1 ~ 100까지의 합 : " + result);
+	}
+}
