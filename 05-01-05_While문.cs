07be8ac929168f3557ed0b2c267e913cdb5ff40b
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-07-29 13:57:56 +0900

    Add files via upload

---
+++ 05-01-05_While문.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// while문을 이용해서 1부터 10까지의 곱셈값을 출력해주세요.
+
+// 10! : 1 x 2 x 3 x 4 x 5 x 6 x 7 x 8 x 9 x 10 = 3628800
+
+class _05_While문
+{
+	static void Main()
+	{
+		int count = 1;
+
+		int result = 1;
+
+		while (count < 11) result *= count++;
+
+		Console.WriteLine("10! = " + result);
+	}
+}
