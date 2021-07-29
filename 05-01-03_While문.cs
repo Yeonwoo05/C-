2aa0a8c718f7418f37e490433b93f7e3382f3cb3
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-07-29 13:57:39 +0900

    Add files via upload

---
+++ 05-01-03_While문.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class _03_While문
+{
+	static void Main()
+	{
+		int A = 0;
+		// > A의 값이 3 미만이라면 반복시킵니다.
+		while (A < 3)
+		{
+			int B = 0;
+			Console.WriteLine("A = " + A);
+			// > B의 값이 3 미만이라면 반복시킵니다.
+			while (B < 3)
+			{
+				Console.WriteLine("		B = " + B);
+				++B;
+			}
+			A++;
+		}
+	}
+}
