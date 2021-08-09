fa229b7f00d12adaf500b68a4da8491c4d563d59
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-08-09 12:59:30 +0900

    Add files via upload

---
+++ 05-03-04_For문.cs
@@ -0,0 +1,60 @@
+﻿using System;
+class _04_For문
+{
+	// *
+	// * *
+	// * * *
+	// * * * * 
+	// * * * * *
+
+	static void Main()
+	{
+		// 개행용 for문
+		for (int i = 0; i < 5; i++)
+		{
+			// 출력용 for문
+			for (int j = 0; j < i + 1; j++)
+				Console.Write(" *");
+			Console.WriteLine();
+		}
+		Console.WriteLine();
+
+		// 2중 포문
+		// * * * * *
+		// * * * * 
+		// * * *
+		// * *
+		// *
+		for (int i = 0; i < 5; i++)
+		{
+			// 출력용 for문
+			for(int j = 0; j < 5 - i; j++)
+			{
+				Console.Write(" *");
+			}
+			Console.WriteLine();
+		}
+		Console.WriteLine();
+
+
+
+		// 2중 포문(개행 포문 안에 포문이 2개가 들어있음)
+		//         *
+		//       * * *
+		//     * * * * *
+		//   * * * * * * *
+		// * * * * * * * * *
+		
+		// 개행용 for문
+		for(int i =0;i < 5; i++)
+		{
+			// 공백용
+			for (int j = 1; j < 5 - i; j++)
+				Console.Write("  ");
+			//출력용
+			for (int k = 0; k < (i * 2) + 1; k++)
+				Console.Write(" *");
+			Console.WriteLine();
+		}
+	}
+}
