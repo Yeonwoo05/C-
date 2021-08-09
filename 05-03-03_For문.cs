c6d4acc0aed84093fde1db0f679c42ba314bd3f3
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-08-09 12:59:23 +0900

    Add files via upload

---
+++ 05-03-03_For문.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class _03_For문
+{ 
+	// For문을 이용해서 10!의 결과문을 출력해주세요.
+
+	static void Main()
+	{
+		int res = 1;
+		for (int i = 1; i < 11; i++)
+			res *= i;
+
+		Console.WriteLine("10! = " + res);
+	}
+}
