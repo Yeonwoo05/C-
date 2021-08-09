1a74a8c5cbcd737409134c6321b7f5d2677d7b9d
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-08-09 12:59:15 +0900

    Add files via upload

---
+++ 05-03-02_For문.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class _02_For문
+{
+	// for문을 이용해서 1 ~ 20까지의 합을 구해보세요.
+
+	static void Main()
+	{
+		// 합계를 저장할 변수
+		int result = 0;
+
+		// for문을 통해서 1 ~ 20까지 값을 더합니다.
+		for(int num = 0; num < 21; num++)
+		{
+			result += num;
+			// result = result + num
+		}
+			Console.WriteLine("1 ~ 20까지의 합 : " + result);
+	}
+}
