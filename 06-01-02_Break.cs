19ff68e20f23d6123dd7b4ed637e74501175ec0d
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-08-23 08:20:51 +0900

    Add files via upload

---
+++ 06-01-02_Break.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+class _02_Break
+{
+	static void Main()
+	{
+		// 사용자 의 입력을 받아 저장할 변수
+		string name;
+		while (true)
+		{
+			Console.WriteLine("\"철수\" 를 입력해주세요.");
+			name = Console.ReadLine();
+
+			// name의 값이 "철수"라면
+			// if(name=="철수")
+			if (name.Equals("철수"))
+				break;
+			// 아니라면
+			else Console.WriteLine("잘못 입력했습니다.");
+		}
+		Console.WriteLine($"{name}를 입력하셨습니다.");
+	}
+}
