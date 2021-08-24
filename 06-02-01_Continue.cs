66a29eaacd950ccb5c62d4f91dd6668eaa83014d
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-08-24 14:17:11 +0900

    Add files via upload

---
+++ 06-02-01_Continue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class _01_Continue
+{
+	// > Continue문
+	/// - 현재 실행중인 반복문을 한번 건너뛰고 반복을 계속 하고자할 때 사용합니다.
+	/// 
+	static void Main()
+	{
+		for(int i = 0; i < 5; i++)
+		{
+			if (i == 3) continue;
+
+			Console.WriteLine("i = " + i);
+		}
+	}
+}
