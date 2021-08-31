559edc8358fde8aae915f2dc1168aeb96baa0131
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-08-31 21:14:27 +0900

    Add files via upload

---
+++ 07-05-01_참조에의한매개변수전달.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class _01_참조에의한매개변수전달
+{
+	// 매개변수 value에 전달 된 값에 10을 더하는 메서드
+	static void AddTen(int value)
+	{
+		value += 10;
+	}
+
+	static void Main()
+	{
+		int A = 0;
+		AddTen(A);
+		Console.WriteLine("A = " + A);
+		// Call By Value ("값에 의한 호출"), Pass By Value("값에 의한 전달")
+		// 값 복사
+	}
+}
