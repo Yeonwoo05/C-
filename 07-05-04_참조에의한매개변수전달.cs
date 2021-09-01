3531bd509a084c615f6c7f8fa70a02b1d9431ca4
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-09-01 20:15:34 +0900

    Add files via upload

---
+++ 07-05-04_참조에의한매개변수전달.cs
@@ -0,0 +1,30 @@
+﻿using System;
+// > ref 한정자를 이용해서 두개의 매개 변수에 전달된 변수의 값을 변경하는
+// Swap() 메서드를 작성해주세요
+// - 조건 : Swap 메서드의 반환 형식은 void로 작성하세요
+
+	// ex ) 
+	// A = 10
+	// B = 20;
+	// Swap (10, 20_
+
+	// A = 20
+	// B = 10
+class _04_참조에의한매개변수전달
+{
+	static void Swap(ref int val1, ref int val2)
+	{
+		int temp = val1;
+		val1 = val2;
+		val2 = temp;
+	}
+
+	static void Main()
+	{
+		int A = 10;
+		int B = 20;
+		Swap(ref A, ref B);
+		Console.WriteLine("A = " + A);
+		Console.WriteLine("B = " + B);
+	}
+}
