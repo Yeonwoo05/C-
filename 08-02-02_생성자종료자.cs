d1ec7662a741d3597fa43820832d200cfd934a9d
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-09-06 20:51:05 +0900

    Add files via upload

---
+++ 08-02-02_생성자종료자.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class _02_생성자종료자
+{
+	int number = 10;
+
+	static void Main() { new _02_생성자종료자().Entry(); }
+
+	void Entry()
+	{
+		Console.WriteLine("Entry() 실행되었습니다.");
+
+		number = 100;
+		Console.WriteLine("number = " + number);
+	}
+}
