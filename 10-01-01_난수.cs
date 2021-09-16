205a714512dc04d739e5d5297802a769ee772b42
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-09-16 16:36:56 +0900

    Add files via upload

---
+++ 10-01-01_난수.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class _01_난수
+{
+	private static void Main()
+	{
+		// 난수란?
+		// - 특정한 배열 순서나, 규칙을 가지지 않는 연속적인 임의의 수를 의미합니다.
+		Random rand = new Random();
+
+		// rand.Next(min, max) : min ~ max - 1까지의 난수를 반환
+		for(int i = 0; i < 5; i++)
+		{
+			Console.WriteLine("뽑은 숫자 : " + rand.Next(0, 5));
+		}
+	}
+}
