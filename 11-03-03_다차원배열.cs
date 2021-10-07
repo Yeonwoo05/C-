960a1081c05d75588c90a5bc0e01e9d0d7090b25
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-10-07 20:45:37 +0900

    Add files via upload

---
+++ 11-03-03_다차원배열.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// 3차원 배열을 하나 만든 뒤
+// 1 ~ 24까지 출력해주세요
+class _03_다차원배열
+{
+	private static void Main() { new _03_다차원배열().Entry(); }
+	private void Entry()
+	{
+		int[,,] MyArray = new int[2, 3, 4];
+		int count = 0;
+		for(int i = 0;i < MyArray.GetLength(0); i++)
+		{
+			for(int j = 0; j < MyArray.GetLength(1); j++)
+			{
+				for(int k = 0; k < MyArray.GetLength(2); k++)
+				{
+					MyArray[i, j, k] = ++count;
+					int elemvalue = MyArray[i, j, k];
+					Console.Write("[ " + (elemvalue < 10 ? " " : "") + elemvalue + " ] ");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+		}
+
+
+	}
+}
+
