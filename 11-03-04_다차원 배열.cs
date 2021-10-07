9e56904746d477beaaa733b01e3f5d60d93c0a66
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-10-07 20:45:46 +0900

    Add files via upload

---
+++ 11-03-04_다차원 배열.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace _11_배열._03_다차원배열
+{
+	class _04_다차원_배열
+	{
+		private static void Main() { new _04_다차원_배열().Entry(); }
+
+		private void Entry()
+		{
+			int[,] arr = new int[4, 5];
+
+			int count = 0;
+
+			for (int i = 0; i < arr.GetLength(0); i++)
+			{
+				for (int j = 0; j < arr.GetLength(1); j++)
+				{
+					arr[i, j] = ++count;
+				}
+			}
+
+			// 출력
+			for (int i = 0; i < arr.GetLength(0); i++)
+			{
+				for (int j = 0; j < arr.GetLength(1); j++)
+				{
+					Console.Write((arr[i, j] < 10 ? " " : "") + arr[i, j] + " ");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			// 뒤에서부터 출력
+
+			for (int i = arr.GetLength(0) - 1; i >= 0; --i)
+			{
+				for (int j = arr.GetLength(1) - 1; j >= 0; --j)
+				{
+					Console.Write((arr[i, j] < 10 ? " " : "") + arr[i, j] + " ");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
